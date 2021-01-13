28fc3f5866da70ae4922b16439d8f51c32a41c80
Dalonte Griffin <dalonteg@yahoo.com>
2021-01-12 19:10:03 -0500

    initial check in

---
+++ helloworld/src/main/java/com/company/Hello.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * @author dalontegriffin
+ *
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!!!");
+    }
+
+
+}
